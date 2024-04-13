993d9c2490736ebc6c428681f8d885b416e2eec2
Mark Corbin <corb0140@algonquinlive.com>
2024-04-13 13:09:44 -0400

    show pages based on wether token is available

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -4,34 +4,44 @@ import Link from "next/link";
 import { logout } from "@/app/actions";
 import { redirect } from "next/navigation";
 
-const Navbar = () => {
+const Navbar = ({ token }) => {
   return (
     <div className={styles.container}>
-      <navbar className={styles.navbar}>
-        <Link className={styles.link} href="/crap">
-          Search 4 Crap
-        </Link>
-        <Link className={styles.link} href="/offer">
-          Offer crap
-        </Link>
-        <Link className={styles.link} href="/mine">
-          My crap
-        </Link>
-        <Link className={styles.link} href="/wiped">
-          Wiped
-        </Link>
+      {!token?.value && (
+        <navbar className={`${styles.navbar} ${styles.center}`}>
+          <Link className={styles.link} href="/crap">
+            Search 4 Crap
+          </Link>
+        </navbar>
+      )}
 
-        <form
-          action={async () => {
-            "use server";
+      {token?.value && (
+        <navbar className={styles.navbar}>
+          <Link className={styles.link} href="/crap">
+            Search 4 Crap
+          </Link>
+          <Link className={styles.link} href="/offer">
+            Offer crap
+          </Link>
+          <Link className={styles.link} href="/mine">
+            My crap
+          </Link>
+          <Link className={styles.link} href="/wiped">
+            Wiped
+          </Link>
 
-            await logout();
-            redirect("/");
-          }}
-        >
-          <button className={styles.logout}>Log out</button>
-        </form>
-      </navbar>
+          <form
+            action={async () => {
+              "use server";
+
+              await logout();
+              redirect("/");
+            }}
+          >
+            <button className={styles.logout}>Log out</button>
+          </form>
+        </navbar>
+      )}
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,73 +1,80 @@
 import Navbar from "./components/Navbar/Navbar";
 import styles from "./page.module.css";
 import { redirect } from "next/navigation";
+import { getSessions } from "@/app/actions";
 
-export default function Home() {
+export default async function Home() {
   const redirectUrl = "http://localhost:3000/login";
   const url = `https://madd9124-finalproject.onrender.com/auth/google?redirect_url=${redirectUrl}`;
+
+  let token = await getSessions();
 
   return (
     <div>
       <nav className={styles.nav}>
-        <Navbar />
+        <Navbar token={token} />
       </nav>
 
-      <div className={styles.container}>
-        <form
-          action={async (formData) => {
-            "use server";
-            redirect(url);
-          }}
-        >
-          <button className={styles.login}>Login</button>
-        </form>
-      </div>
+      {!token?.value && (
+        <div className={styles.container}>
+          <form
+            action={async () => {
+              "use server";
+              redirect(url);
+            }}
+          >
+            <button className={styles.login}>Login</button>
+          </form>
+        </div>
+      )}
 
-      {/* <div className={styles.container}>
-        <form className={styles.form} action="">
-          <div className={styles.formBox}>
-            <label className={styles.label} htmlFor="">
-              Search for Crap
-            </label>
-            <input
-              className={styles.searchField}
-              type="text"
-              placeholder="keyword"
-            />
-          </div>
+      {token?.value && (
+        <div className={styles.container}>
+          <form className={styles.form} action="">
+            <div className={styles.formBox}>
+              <label className={styles.label} htmlFor="">
+                Search for Crap
+              </label>
+              <input
+                className={styles.searchField}
+                type="text"
+                placeholder="keyword"
+              />
+            </div>
 
-          <div className={styles.formBox}>
-            <p className={styles.radioHeading}>Please enter a range with: </p>
+            <div className={styles.formBox}>
+              <p className={styles.radioHeading}>Please enter a range with: </p>
 
-            <div className={styles.radio}>
-              <div>
-                <input type="radio" id="1000meters" value="1000" />
-                <label className={styles.label} htmlFor="1000meters">
-                  1000 Meters
-                </label>
-              </div>
+              <div className={styles.radio}>
+                <div>
+                  <input type="radio" id="1000meters" value="1000" />
+                  <label className={styles.label} htmlFor="1000meters">
+                    1000 Meters
+                  </label>
+                </div>
 
-              <div>
-                <input type="radio" id="1000meters" value="3000" />
-                <label className={styles.label} htmlFor="3000meters">
-                  3000 Meters
-                </label>
-              </div>
+                <div>
+                  <input type="radio" id="1000meters" value="3000" />
+                  <label className={styles.label} htmlFor="3000meters">
+                    3000 Meters
+                  </label>
+                </div>
 
-              <div>
-                <input type="radio" id="1000meters" value="5000" />
-                <label className={styles.label} htmlFor="5000meters">
-                  5000 Meters
-                </label>
+                <div>
+                  <input type="radio" id="1000meters" value="5000" />
+                  <label className={styles.label} htmlFor="5000meters">
+                    5000 Meters
+                  </label>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className={styles.formBox}>
-            <input className={styles.submit} type="submit" />
-          </div>
-        </form>
-      </div> */}
+            <div className={styles.formBox}>
+              <input className={styles.submit} type="submit" />
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
