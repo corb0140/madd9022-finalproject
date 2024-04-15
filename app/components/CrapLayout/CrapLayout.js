0b175195f0ea3a563fdea12bd2c8468a9fdd9e1a
Mark Corbin <corb0140@algonquinlive.com>
2024-04-15 14:03:24 -0400

    update crap layout styles

--- app/components/CrapLayout/CrapLayout.js
+++ app/components/CrapLayout/CrapLayout.js
@@ -1,6 +1,14 @@
+"use client";
+
 import styles from "./CrapLayout.module.css";
+import { useRouter } from "next/navigation";
 
 const CrapLayout = ({ data }) => {
+  const router = useRouter();
+
+  const navigate = (id) => {
+    router.push(`/crap/${id}`);
+  };
   const craps = data.data;
   return (
     <div className={styles.container}>
@@ -9,7 +17,11 @@ const CrapLayout = ({ data }) => {
           craps.map((crap) => {
             console.log(crap);
             return (
-              <div className={styles.card} key={crap._id}>
+              <div
+                className={styles.card}
+                key={crap._id}
+                onClick={() => navigate(crap._id)}
+              >
                 <div className={styles.cardTitle}>{crap.title}</div>
                 <div className={styles.cardDescription}>{crap.description}</div>
               </div>
